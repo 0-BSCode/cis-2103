8b139aefe39d84e37b9969d5bc22b732ee45263e
Bryan Sanchez <bryanipsanchez@gmail.com>
2023-09-08 09:55:41 +0800

    Test transfer method

--- BankAccount/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ BankAccount/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -13,11 +13,23 @@ public class JavaApplication1 {
 
     public static void main(String[] args) {
         BankAccount newAccount = new BankAccount(1, "Bryan", "bryan@gmail.com", "01234567");
+        BankAccount newAccount2 = new BankAccount(2, "Christian", "christian@gmail.com", "01234567");
+       
+//        System.out.println(newAccount.depositFunds(69.20f));
+//        newAccount.withdrawFunds(69.0f);
+//        System.out.println(newAccount.withdrawFunds(69.0f));
+//        System.out.println(newAccount.depositFunds(-2.0f));
+
+        newAccount.depositFunds(69.42f);
         
-        System.out.println(newAccount.depositFunds(69.20f));
-        newAccount.withdrawFunds(69.0f);
-        System.out.println(newAccount.withdrawFunds(69.0f));
-        System.out.println(newAccount.depositFunds(-2.0f));
+        System.out.printf("Account 1 OB: %.2f\n", newAccount.getAccountBalance());
+        System.out.printf("Account 2 OB: %.2f\n", newAccount2.getAccountBalance());
+
+        System.out.println(newAccount.transfer(newAccount2, 45.0f));
+        
+        System.out.printf("Account 1 NB: %.2f\n", newAccount.getAccountBalance());
+        System.out.printf("Account 2 NB: %.2f\n", newAccount2.getAccountBalance());
+
 
     }
 }
